a8c40dda7e4c4baed05627a5ac3e08fecc351f9c
牧大介 <raki0401@outlook.jp>
2021-01-09 21:31:51 +0900

    完成

--- js/script.js
+++ js/script.js
@@ -1,71 +1,78 @@
 
 //drawer
-$(function(){
+$(function () {
   //drawerjs
   $('.drawer').drawer();
 })
 
 
 //スムーススクロール
-$(function(){
-  $('a[href^="#"]').click(function(){
+$(function () {
+  $('a[href^="#"]').click(function () {
     let header = $(".header").innerHeight();
     let speed = 700;
     let id = $(this).attr("href");
     let target = $("#" == id ? "html" : id);     // 三項演算子
     let position = $(target).offset().top - header;
-    $("html,body").animate({scrollTop : position  },speed);
+    $("html,body").animate({ scrollTop: position }, speed);
     return false;
   });
 });
 
 //wow-js
-$(function(){
+$(function () {
   new WOW().init();
 });
 
 //google form
-$(function(){
+$(function () {
 
   let $form = $('#js-form');
-  $form.submit(function(e) { 
-    $.ajax({ 
-     url: $form.attr('action'), 
-     data: $form.serialize(), 
-     type: "POST", 
-     dataType: "xml", 
-     statusCode: { 
-        0: function() { 
+  $form.submit(function (e) {
+    $.ajax({
+      url: $form.attr('action'),
+      data: $form.serialize(),
+      type: "POST",
+      dataType: "xml",
+      statusCode: {
+        0: function () {
           //送信に成功したときの処理 
           $form.slideUp()
           $('#js-success').slideDown()
-        }, 
-        200: function() { 
+        },
+        200: function () {
           //送信に失敗したときの処理 
           $form.slideUp()
           $('#js-error').slideDown()
-        } 
-      } 
+        }
+      }
     });
-    
+
     e.preventDefault();
-    return false; 
-  }); 
+    return false;
+  });
 });
 
 
 
 // formの入力
-// let submit = $('#js-submit')
-// $('#js-form input, #js-form textarea').on('change', function(){
-//   if(
-//     $('#js-form input[type="text"]').val() !== "" &&
-//     $('#js-form input[type="email"]').val() !== "" &&
-//     $('#js-form input[name="entry.577350080"]').prop('checked') === true
-//   ){
-//     $submit.addClass('-active')
-//   } else{
-//     $submit.removeClass('-active')
+$(function () {
+  let $submit = $('#js-submit');
+  $('#js-form input, #js-form textarea').on('change', function () {
+    if (
+      $('#js-form input[type="text"]').val() !== "" &&
+      $('#js-form input[type="email"]').val() !== "" &&
+      $('#js-form input[name="entry.197829066"]').prop('checked') === true &&
+      $('#js-form textarea[name="entry.890350701"]').val() !== ""
+    ) {
+      // 全て入力されたとき
+      $submit.prop('disabled',false)
+      $submit.addClass('-active') 
+    } else {
+      // 入力されていないとき
+      $submit.removeClass('-active')
+      $submit.prop('disabled',true)
 
-//   }
-// });
+    }
+  });
+});
